53d7c88c7842fae4b3b6d1b25a683caba8119fbb
studioValenz <studiovalenz@gmail.com>
2023-05-25 19:35:08 -0300

    Criado a função para mostrar a lista na tela

--- script.js
+++ script.js
@@ -4,6 +4,18 @@ let buttonElement = document.querySelector("#app button");
 
 let tarefas = [];
 
+let renderTarefas = () => {
+  listElement.innerHTML = "";
+
+  tarefas.map((todo) => {
+    let liElement = document.createElement("li");
+    let tarefaText = document.createTextNode(todo);
+
+    liElement.appendChild(tarefaText);
+    listElement.appendChild(liElement);
+  });
+};
+
 let adcionarTarefas = () => {
   if (inputElement.value === "") {
     alert("Digite alguma tarefa");
@@ -13,6 +25,8 @@ let adcionarTarefas = () => {
 
     tarefas.push(novaTarefa);
     inputElement.value = "";
+
+    renderTarefas();
   }
 };
 
